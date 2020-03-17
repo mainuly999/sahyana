166fda0f59347a6665b7919ab05deadf042ffbfd
mainuly999 <mainuly999@gmail.com>
2020-03-17 14:10:57 +0800

    integrasi backend-DB-frontEnd using restAPI too dan sedikit perubahan vue pada frontend

--- src/components/WomanShayna.vue
+++ src/components/WomanShayna.vue
@@ -4,11 +4,12 @@
     <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12 mt-5">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
                     <carousel class="product-slider" v-bind:items="3" :dots="false" :nav="false" :autoplay="true">
-                        <div class="product-item">
+                        <div class="product-item" v-for="itemProduct in products" v-bind:key="itemProduct.id">
+                            <!-- jadi pada products dibawahkan diambil dari json APInya yand data.data.data nah disitu kita akan ambil id pada data ketiga sebagai id di class="product-item", untuk check silahkan chekc link api yang ada pada munted dibawah  -->
                             <div class="pi-pic">
-                                <img src="img/mickey1.jpg" alt="" />
+                                <img v-bind:src="itemProduct.galleries[0].photo" alt="" />
                                 <ul>
                                     <li class="w-icon active">
                                         <a href="#"><i class="icon_bag_alt"></i></a>
@@ -17,80 +18,23 @@
                                 </ul>
                             </div>
                             <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
+                                <!-- kuranglebih sama dengan penjelasan diatas  -->
+                                <div class="catagory-name">{{itemProduct.type}}</div>
                                 <a href="#">
-                                    <h5>Mickey Baggy</h5>
+                                    <h5>{{itemProduct.name}}</h5>
                                 </a>
                                 <div class="product-price">
-                                    $14.00
+                                    ${{itemProduct.price}}
                                     <span>$35.00</span>
                                 </div>
                             </div>
                         </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
+
                     </carousel>
+                </div>
+
+                <div class="col-lg-12" v-else>
+                    <p>Product terbaru belum tersedia</p>
                 </div>
             </div>
         </div>
@@ -99,14 +43,31 @@
 </template>
 
 <script>
-import carousel from 'vue-owl-carousel'
+import carousel from 'vue-owl-carousel';
+// setelah install npm axios maka
+import axios from "axios";
 
 export default {
     name:'WomanShayna',
     components:{
         carousel
+    },
+    data(){
+        return{
+            products:[]
+        }
+    },
+    // ambil dari API
+    mounted(){
+        // get(url yang ada pada psotman bagian product)
+        axios.get("http://127.0.0.1:8000/api/products")
+            // kemudain res itu untnutk mengambi data diatas, nah kenpa 3 datanya karena data pertama itu general seperti meta,data,dll kemudian data kedua adalah spesifik dnegna nama data begipula data ketiga
+            .then(res=>(this.products = res.data.data.data))
+            .catch(err=>console.log(err));
+
+        
     }
-}
+};
 </script>
 
 <style scoped>
